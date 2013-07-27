07a63fc2140cae1776414d6e5b8ce3900abb3a04
sidhantx <sidhantx@gmail.com>
2013-07-27 01:54:49 +0000

    fs require

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
 
 var buffer = new Buffer(256);
