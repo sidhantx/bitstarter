3b59236157cc1ceb12e86f0408a03d636354ae19
sidhantx <sidhantx@gmail.com>
2013-07-27 01:51:04 +0000

    Used buffer to print

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+var buffer = new Buffer(256);
+buffer =fs.readFileSync("index.html","utf-8");
+var str = buffer.toString();
+
+response.send(str);
 });
 
 var port = process.env.PORT || 5000;
